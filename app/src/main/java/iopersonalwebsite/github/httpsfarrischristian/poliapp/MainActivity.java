140bbaf58eb2b8dd4901d45666c456b788d4134e
Christian Farris <cfarris@wisc.edu>
2018-09-28 17:35:25 -0500

    first commit

---
+++ app/src/main/java/iopersonalwebsite/github/httpsfarrischristian/poliapp/MainActivity.java
@@ -0,0 +1,13 @@
+package iopersonalwebsite.github.httpsfarrischristian.poliapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
